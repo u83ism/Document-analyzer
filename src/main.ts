86af28060082ccd0d27899dd149b2cfabc5183a3
ゆうやみ(yu-yami) <u83ism@gmail.com>
2024-07-28 12:32:34 +0900

    ➕🔧🚚🙈 モジュール化/ビルドしてのデプロイを整備

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ type MatchInfo = {
 	matchCount: number,
 }
 
-
 function isValidDate(dateString) {
 	// 日付フォーマットの検証をここで行います（例としてYYYY/MM/DDフォーマットを使用）
 	var regex = /^\d{4}\/\d{2}\/\d{2}$/;
@@ -18,9 +17,13 @@ function getMatchCount(text) {
 	return null;
 }
 
-
+/**
+ * ドキュメントのうち、試合ログ以降の部分を取得する
+ * 
+ * @param paragraphs 
+ * @returns 
+ */
 const getMatchLogParagraphs = (paragraphs: ReadonlyArray<GoogleAppsScript.Document.Paragraph>): Array<GoogleAppsScript.Document.Paragraph> => {
-	// 試合ログ以降の部分を取得する
 	const matchLogIndex = paragraphs.findIndex((paragraph => {
 		const heading = paragraph.getHeading();
 		const text = paragraph.getText();
@@ -31,12 +34,29 @@ const getMatchLogParagraphs = (paragraphs: ReadonlyArray<GoogleAppsScript.Docume
 	return matchLogParagraphs
 }
 
-
-const getMatchInfoList = (paragraphs: ReadonlyArray<GoogleAppsScript.Document.Paragraph>): Array<MatchInfo> => {
+export const getMatchInfoList = (paragraphs: ReadonlyArray<GoogleAppsScript.Document.Paragraph>): Array<MatchInfo> => {
 	const matchLogParagraphs = getMatchLogParagraphs(paragraphs)
-	matchLogParagraphs.forEach(paragraph => {
-		const text = paragraph.getText();
+
+	// 
+	const headingToPurposeMap = {
+		title: DocumentApp.ParagraphHeading.HEADING1,
+		date: DocumentApp.ParagraphHeading.HEADING2,
+		matchCount: DocumentApp.ParagraphHeading.HEADING3,
+	} as const
+
+
+	const matchInfoList = []
+
+	matchLogParagraphs.filter(paragraph => {
 		const heading = paragraph.getHeading();
+		// 「試合ログ」そのものはいらない
+		return heading !== headingToPurposeMap.title
+	}).forEach(paragraph => {
+		const heading = paragraph.getHeading();
+		const text = paragraph.getText();
+		if (heading === headingToPurposeMap.date) {
+
+		}
 
 		Logger.log(`heading:${heading}/text:${text}`)
 	})
@@ -68,3 +88,22 @@ const getMatchInfoList = (paragraphs: ReadonlyArray<GoogleAppsScript.Document.Pa
 
 	return [matchInfo]
 }
+
+export const test = (): void => {
+	// 深夜のAmongUs会のドキュメントを取得
+	const documentId = "1jaagzHsuiGC4TSTHuHBOP9cvb4io0xLNl1arlaMr-9";
+	const doc = DocumentApp.openById(documentId);
+
+	const paragraphs = doc.getBody().getParagraphs();
+	paragraphs.forEach(paragraph => {
+		const text = paragraph.getText();
+		const numChildren = paragraph.getNumChildren()
+		Logger.log(`text: ${text}/numChildren: ${numChildren}`)
+
+	})
+
+}
+
+// GASから参照したい関数はglobalオブジェクトに渡してあげる必要がある
+(global as any).getMatchInfoList = getMatchInfoList;
+(global as any).test = test;
