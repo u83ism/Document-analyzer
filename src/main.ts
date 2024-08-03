edf1c77f0e50b9519070235573ea07432206ba1f
ゆうやみ(yu-yami) <u83ism@gmail.com>
2024-08-03 20:49:15 +0900

    👔 平文部分の解析がほぼ完了し、書き込みするためのデータ調整中

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
 import markdownit from "markdown-it";
 
-const extractMatchCount = (text: string): number => {
+type Mode = "クラシック" | "かくれんぼ"
+
+
+const extractMatchCountAndMode = (text: string): [number, Mode] => {
 	// 正規表現で「第」と「試合」の間の数字部分を抽出
 	const match = text.match(/第(\d+)試合/);
 	if (!match) {
-		throw new Error(' "第●試合"という書式である必要があります');
+		throw new Error(`"第●試合"という書式である必要があります。 text:${text}`);
 	}
 	const number = parseInt(match[1], 10);
 	if (isNaN(number)) {
 		throw new Error('試合番号が数字として取得できません');
 	}
-	return number;
+
+	const mode: Mode = text.includes("かくれんぼ") ? "かくれんぼ" : "クラシック"
+
+	return [number, mode];
 }
 
 
@@ -45,46 +51,68 @@ const splitLogAndMovieParts = (texts: Array<string>): Array<Array<string>> => {
 }
 
 
-const separateKeyAndValue = (text: string): [string, string] => {
+const separateKeyAndValue = (text: string): [string | null, string | null] => {
 	const separator = ":"
 	const parts = text.split(separator); // コロンでキーと値を分割
-	Logger.log(`---separate Key And Value---`)
 	if (parts.length < 2) {
 		// コロンがないか、値が空の場合は無視
-		throw new Error(`書式が間違っています。${separator}で区切ってある必要があります。 対象のテキスト:${parts}`);
+		return [null, null]
 	}
-	Logger.log(`---1---`)
 	const key = parts[0].trim();
 	// 文中に":"が有って3つ以上に分かれた場合に備えてコロン以降の部分をすべて結合する
 	const value = parts.slice(1).join(separator).trim();
-	Logger.log(`---2---`)
 	return [key, value]
 }
 
-const parseStringToObject = (text: string): any => {
-	const lines = text.split('\n'); // 改行で行を分割
-	const result: any = {};
 
-	const [logParts, movieParts] = splitLogAndMovieParts(lines)
-
-	logParts.forEach(line => {
-		const [key, value] = separateKeyAndValue(line)
-		result[key] = value;
-	});
-
-	// moviePart.forEach(line => {
-	// 	const [key, value] = separateKeyAndValue(line)
-	// 	result["動画"][key] = value;
-	// })
-	return result;
+// Googleドキュメントで見出し等も設定されていない、普通の文章部分を構造化したもの
+type MatchRawData = {
+	"マップ": string,
+	// 配列化する必要あり
+	"参加者": string,
+	"インポスター": string,
+	"勝敗": string,
+	"概要": string,
+	//【動画】は必ずあるが、その後が空
+	"動画": {
+		[key in string]: string
+	} | null
 }
 
-const extractMatchInfo = (text: string): MatchLog => {
+const isMatchRawData = (data: any): data is MatchRawData => {
+	const propertyNames = ["マップ", "参加者", "インポスター", "勝敗", "概要"]
+	const result = propertyNames.every((propertyNames: string) => {
+		return (data?.[propertyNames] !== undefined && typeof data[propertyNames] === "string")
+	})
+	return result
+}
 
+const parseToMatchRawData = (text: string): MatchRawData | null => {
+	const lines = text.split('\n'); // 改行で行を分割
 
+	const [logParts, movieParts] = splitLogAndMovieParts(lines)
+	const result: any = {};
+	logParts.forEach(line => {
+		const [key, value] = separateKeyAndValue(line)
+		if (key !== null && value !== null) { result[key] = value; }
+	});
 
+	movieParts.forEach(line => {
+		const [key, value] = separateKeyAndValue(line)
+		if (key !== null && value !== null) {
+			result["動画"] = result["動画"] ?? {};
+			result["動画"][key] = value;
+		}
+	})
 
+	if (isMatchRawData(result)) {
+		return result
+	} else {
+		return null
+	}
+}
 
+const getMatchInfo = (date: string, matchcount): MatchLog => {
 	return {}
 }
 
@@ -92,15 +120,6 @@ const extractMatchInfo = (text: string): MatchLog => {
 export const test = (): void => {
 	// 深夜のAmongUs会のドキュメントを取得
 	const documentId = "1jaagzHsuiGC4TSTHuHBOP9cvb4io0xLNl1arlaMr-9I";
-	const doc = DocumentApp.openById(documentId);
-
-	// const paragraphs = doc.getBody().getParagraphs();
-	// paragraphs.forEach(paragraph => {
-	// 	const text = paragraph.getText();
-	// 	const numChildren = paragraph.getNumChildren()
-	// 	Logger.log(`text: ${text}/numChildren: ${numChildren}`)
-
-	// })
 
 	const url = `https://docs.google.com/feeds/download/documents/export/Export?exportFormat=markdown&id=${documentId}`;
 	const response = UrlFetchApp.fetch(url, {
@@ -111,9 +130,11 @@ export const test = (): void => {
 	const mdi = markdownit()
 	const tokens = mdi.parse(mdText, {})
 
-	// 「試合ログ」の段落を探す
-	const matchLogStartIndex = tokens.findIndex(mdInfo => mdInfo.content.includes("試合ログ"))
-	const matchLogTokens = tokens.slice(matchLogStartIndex)
+	// 「試合ログ」の次段落を探す
+	const matchLogStartIndex = tokens.findIndex(mdInfo => mdInfo.content.includes("試合ログ")) + 1
+	// 「（テンプレート）」を探す
+	const matchLogEndIndex = tokens.findIndex(mdInfo => mdInfo.content.includes("（テンプレート）")) + 1
+	const matchLogTokens = tokens.slice(matchLogStartIndex, matchLogEndIndex)
 
 	const matchLogs: Array<MatchLog> = [];
 	// ループを跨いで管理する必要のある状態
@@ -121,15 +142,17 @@ export const test = (): void => {
 		enableTag: string,
 		dateText: string,
 		matchCount: number,
+		mode: Mode,
 	}
 	const state: State = {
 		enableTag: "",
 		dateText: "",
 		matchCount: 0,
+		mode: "クラシック"
 	}
 
 	matchLogTokens.forEach(token => {
-		Logger.log(token)
+		// console.debug(token)
 
 		switch (token.type) {
 			case "heading_open":
@@ -139,7 +162,6 @@ export const test = (): void => {
 				state.enableTag = ""
 				break;
 			case "inline":
-
 				switch (state.enableTag) {
 					case "h2":
 						// 日付（"6/2昼"とかあるのでstringにする必要あり注意）
@@ -147,17 +169,22 @@ export const test = (): void => {
 						break;
 					case "h3":
 						// 試合番号
-						state.matchCount = extractMatchCount(token.content)
+						[state.matchCount, state.mode] = extractMatchCountAndMode(token.content)
 						break;
-					default:
-						// 試合データ
-						const object = parseStringToObject(token.content)
-						Logger.log(object)
+					case "":
+						// 「とりまとめシートの「試合ログ」に移動しました。」部分除け
+						if (state.matchCount !== 0) {
+							// 試合データ
+							const rawData = parseToMatchRawData(token.content)
+							console.info(`---解析結果---`)
+							console.info(rawData)
+							if (rawData === null) {
+								console.warn("★解析に失敗している可能性があります★")
+								console.warn(token)
+							}
+						}
 						break;
 				}
-
-				break;
-			default:
 				break;
 		}
 	})
@@ -165,22 +192,5 @@ export const test = (): void => {
 }
 
 
-
-const testError = () => {
-	throw new Error("Error!!")
-}
-
-const testErrorInNestedFunction = () => {
-	const texts = ["A", "B", "C"]
-	texts.forEach(text => { testError() })
-}
-
-export const testErrorInDobleNestedFunction = () => {
-	const numbers = [1, 2, 3]
-	numbers.forEach((number) => testErrorInNestedFunction())
-}
-
-
 // GASから参照したい関数はglobalオブジェクトに渡してあげる必要がある
 (global as any).test = test;
-(global as any).testErrorInDobleNestedFunction = testErrorInDobleNestedFunction;
